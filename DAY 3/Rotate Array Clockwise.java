7ca99de03753b45452f760866733b48a3d214ae3
pranavgarg13-star <pranavgarg1603@gmail.com>
2025-10-29 22:26:27 +0530

    Create Rotate Array Clockwise.java

---
+++ DAY 3/Rotate Array Clockwise.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotateclockwise(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        k = k % n; 
+
+        
+        reverse(arr, 0, n - 1);
+       
+        reverse(arr, 0, k - 1);
+        
+        reverse(arr, k, n - 1);
+    }
+
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
