1cc0ec970c81f3354e1280c94ab4c63a08bf85dd
pranavgarg13-star <pranavgarg1603@gmail.com>
2025-10-30 21:38:18 +0530

    Create Count Elements With Maximum Frequency.java

---
+++ DAY 4/Count Elements With Maximum Frequency.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+          int[] freq = new int[101]; 
+
+        
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        
+        int maxFreq = 0;
+        for (int f : freq) {
+            if (f > maxFreq)
+                maxFreq = f;
+        }
+
+        
+        int total = 0;
+        for (int f : freq) {
+            if (f == maxFreq)
+                total += f;
+        }
+
+        return total;
+    }
+}
