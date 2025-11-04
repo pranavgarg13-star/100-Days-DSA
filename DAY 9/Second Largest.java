0e8696f0a747abb9b6b6be0a5d2db123b683358c
pranavgarg13-star <pranavgarg1603@gmail.com>
2025-11-04 18:47:50 +0530

    Create Second Largest.java

---
+++ DAY 9/Second Largest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest; 
+                largest = num; 
+            } else if (num > secondLargest && num < largest) {
+                
+                secondLargest = num;
+            }
+        }
+
+    
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+}
