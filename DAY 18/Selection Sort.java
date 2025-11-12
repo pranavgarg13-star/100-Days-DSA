20acd187a71ec3ae726ff14e19cbb03ddfb89335
pranavgarg13-star <pranavgarg1603@gmail.com>
2025-11-12 18:42:34 +0530

    Create Selection Sort.java

---
+++ DAY 18/Selection Sort.java
@@ -0,0 +1,23 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        
+       int n = arr.length;
+
+        
+        for (int i = 0; i < n - 1; i++) {
+            
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
